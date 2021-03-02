e9b1d47c9e164ab7c38dbd6493871b856a9ae6ad
Ayush Bansal <bayush611@gmail.com>
2021-03-02 20:02:55 +0530

    Stdin and Stdout II
     Print the string after the white space.

---
+++ Java/Problem_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i1 = scan.nextInt();
+        double d =scan.nextDouble();
+
+        scan.nextLine();          // to print the string after the white space
+        String s = scan.nextLine();
+
+        System.out.println("String: " +s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i1);
+    }
+}
