2a5fb6d3bf5b6e02fbfd9e3b66380d0572dcb525
Ayush Bansal <bayush611@gmail.com>
2021-03-05 11:57:53 +0530

    1D Array
    When we save each integer to its corresponding index in a , we get a=[10,20,30,40,50]. The locked code prints each array element on a new line from left to right.

---
+++ Java/Problem_12.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Problem_12{
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            a[i]=scan.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+
+        }
+    }
+}
